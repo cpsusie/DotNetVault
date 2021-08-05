124c1c244182d3665d0196433e64d835098c5954
Christopher P. Susie <cpsusie@hotmail.com>
2021-08-05 18:11:20 -0400

    Fixed problem where stray logger causing unit tests to remain open too long; Added uri to whitelist so unit test passes.

--- DotNetVault/Logging/LoggerImpl.cs
+++ DotNetVault/Logging/LoggerImpl.cs
@@ -38,7 +38,57 @@ namespace DotNetVault.Logging
             }
         }
 
+        public static (bool LoggerExisted, bool LoggerDestroyedByThisCall) DestroyLogger()
+        {
+            bool loggerDestroyedThisCall;
+            ILogProvider provider = _provider;
+            bool loggerExisted = provider != null;
+            if (loggerExisted && _destroyed.TrySet())
+            {
+                provider.Dispose();
+                loggerDestroyedThisCall = true;
+            }
+            else
+            {
+                loggerDestroyedThisCall = false;
+            }
 
+            return (loggerExisted, loggerDestroyedThisCall);
+        }
+
+        private static LocklessLazySetOnce _destroyed;
         private static volatile ILogProvider _provider;
+    }
+
+    internal struct LocklessLazySetOnce
+    {
+        public static implicit operator bool(in LocklessLazySetOnce convert) => convert.IsSet;
+        public readonly bool IsSet
+        {
+            get
+            {
+                int code = _code;
+                return code == Set;
+            }
+        }
+
+        public bool TrySet()
+        {
+            const int wantToBe = Set;
+            const int needToBeNow = NotSet;
+            return Interlocked.CompareExchange(ref _code, wantToBe, needToBeNow) == needToBeNow;
+        }
+
+        public void SetOrThrow()
+        {
+            if (!TrySet()) 
+                throw new InvalidOperationException("Flag already set.");
+        }
+
+        public override readonly string ToString() => $"Set once flag status: [{(IsSet ? "SET" : "CLEAR")}].";
+
+        private volatile int _code;
+        private const int NotSet = 0;
+        private const int Set = 1;
     }
 }
