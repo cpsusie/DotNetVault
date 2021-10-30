315c18e1f70f12443bb262978f3421a60e09e9db
Christopher Susie <cpsusie@hotmail.com>
2021-10-30 09:32:46 -0400

    Imported updates from v1.0_master:     upgrade (HpTimeStamps minv- 1.0.0.6)     fix problem with basic monitor vault when initial value not supplied to ctor.

--- VaultUnitTests/VaultAcqBehaviorTest.cs
+++ VaultUnitTests/VaultAcqBehaviorTest.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using DotNetVault.Exceptions;
 using JetBrains.Annotations;
 using Xunit;
 using Xunit.Abstractions;
-
+using ResourceType = System.String;
+using NullVtType = System.Nullable<ulong>;
+using VtType = System.UInt64;
 namespace VaultUnitTests
 {
+    using VaultType = DotNetVault.Vaults.BasicMonitorVault<ResourceType>;
+    using NullVtVault = DotNetVault.Vaults.BasicMonitorVault<NullVtType>;
+    using VtVault = DotNetVault.Vaults.BasicMonitorVault<VtType>;
     public abstract class VaultAcqBehaviorTest : TestBase<VaultFactoryFixture>
     {
+
+
         protected VaultAcqBehaviorTest([NotNull] ITestOutputHelper helper, [NotNull] VaultFactoryFixture fixture) : base(helper, fixture)
         {
         }
@@ -23,4 +31,39 @@ namespace VaultUnitTests
             Fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
         }
     }
+
+    public class NullableVtVaultFactoryFixture : VaultFactoryFixture
+    {
+        public TimeSpan VaultTimeout
+        {
+            get => _timeout;
+            init => _timeout = value > TimeSpan.Zero
+                ? value
+                : throw new ArgumentNotPositiveException<TimeSpan>(nameof(value), value);
+        }
+
+        public NullVtVault CreateSetNullVtVault(NullVtType initial) => new(initial, _timeout);
+
+        public NullVtVault CreateUnsetNullableVtVault() => new(_timeout);
+
+        public VtVault CreateUnsetVtVault() => new(_timeout);
+
+        public VtVault CreateSetVtVault(VtType value) => new(value, _timeout);
+
+        public VaultType CreateUnsetVault() => new(_timeout);
+
+        public VaultType CreateSetVault(ResourceType rt) => new(rt, _timeout);
+
+
+        private readonly TimeSpan _timeout = TimeSpan.FromMilliseconds(500);
+    }
+
+    public abstract class NullableAcqTests : TestBase<NullableVtVaultFactoryFixture>
+    {
+        /// <inheritdoc />
+        protected NullableAcqTests([NotNull] ITestOutputHelper helper,
+            [NotNull] NullableVtVaultFactoryFixture fixture) : base(helper, fixture) { }
+    }
+
+
 }
